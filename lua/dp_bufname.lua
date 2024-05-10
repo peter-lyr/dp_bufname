c15a343ae8b3500f2565c820f1ef1f46b5526faf
peter-lyr <llydrp.ldp@gmail.com>
2024-05-10 23:39:23 +0800

    function M.decrypt_secret()

--- lua/dp_bufname.lua
+++ lua/dp_bufname.lua
@@ -21,11 +21,21 @@ function M.encrypt()
   B.encrypt()
 end
 
+function M.decrypt_secret()
+  B.decrypt_secret()
+end
+
+function M.encrypt_secret()
+  B.encrypt_secret()
+end
+
 require 'which-key'.register {
   ['<leader>b'] = { name = 'bufname', },
   ['<leader>bc'] = { name = 'bufname.crypt', },
   ['<leader>bcd'] = { function() M.decrypt() end, 'bufname.crypt: decrypt', mode = { 'n', 'v', }, silent = true, },
   ['<leader>bce'] = { function() M.encrypt() end, 'bufname.crypt: encrypt', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>bc<leader>d'] = { function() M.decrypt_secret() end, 'bufname.crypt: decrypt_secret', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>bc<leader>e'] = { function() M.encrypt_secret() end, 'bufname.crypt: encrypt_secret', mode = { 'n', 'v', }, silent = true, },
 }
 
 return M
